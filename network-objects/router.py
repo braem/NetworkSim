d9f4880c2a043d7464df34f6c8667ff6c49dd46b
bolster <bolster@unbc.ca>
2016-11-11 14:58:56 -0800

    Network objects prototypes complete
    Network objects are complete. They need the frame unwrapping and
    wrapping but that is someone else job.

--- network-objects/router.py
+++ network-objects/router.py
@@ -1,14 +1,14 @@
-import Node
-class Router (Node):
-
+import Switch
+class Router (Switch):
     def __init__(self):
         self.node_id = self.Node.node_id
         self.Node.node_id += 1
 
-        
+    def get_ip_frame(self, message):
+        #TODO this
+        pass
 
 
-
-
-
-
+    def wrap_new_ip_frame(self, message, destination_id):
+        #TODO this
+        pass
--- network-objects/switch.py
+++ network-objects/switch.py
@@ -1,2 +1,14 @@
-class Switch:
+import Node
+class Switch (Node):
     def __init__(self):
+        self.node_id = self.Node.node_id
+        self.Node.node_id += 1
+
+    def get_ethernet_frame(self, message):
+        #TODO this
+        pass
+
+
+    def wrap_new_ethernet_frame(self, message, destination_id):
+        #TODO this
+        pass
