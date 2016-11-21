5566c2fb1b56cca64811efbf31fe40eaa898dd41
dub71ca <dub71ca@gmail.com>
2016-11-21 12:06:33 -0800

    Improved Nodes and connections collections - better node ID

--- src/Node.py
+++ src/Node.py
@@ -1,3 +1,6 @@
+import string, time, math, random
+
+
 """Node.py - node object """
 __author__ = "Darren Hendrickson"
 __version__ = "1.0.0"
@@ -7,9 +10,9 @@ class Node(object):
 
     counter = 0
 
-    def __init__(self, node_id, nodeType, x, y):
+    def __init__(self, nodeType, x, y):
         Node.counter += 1
-        self.__Node__ID = node_id           # instance variable unique to each instance
+        self.__Node_ID = self.uniqid()           # instance variable unique to each instance
         self.__NodeType = nodeType          # node type "PC", "Router", or "Switch"
         self.__X = x                        # position x value
         self.__Y = y                        # position y value
@@ -17,6 +20,17 @@ class Node(object):
 
     def __del__(self):
         Node.counter -= 1
+
+    def uniqid(more_entropy=False):
+        m = time.time()
+        uniqid = '%8x%05x' % (math.floor(m), (m - math.floor(m)) * 1000000)
+        if more_entropy:
+            valid_chars = list(set(string.hexdigits.lower()))
+            entropy_string = ''
+            for i in range(0, 10, 1):
+                entropy_string += random.choice(valid_chars)
+            uniqid = uniqid + entropy_string
+        return uniqid
 
     def setType(self, nType):
         self.__NodeType = nType
@@ -36,3 +50,4 @@ class Node(object):
 
     def getNetworkInfo(self):
         return self.__NetworkInfo
+
