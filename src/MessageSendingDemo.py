59c31b014892816e11898e40bf93dadcb10dbb4c
rhdabe <rhdabe@gmail.com>
2016-11-21 17:52:31 -0800

    Removed two host demo as it is now obsolete.  Some docstring stuff.

--- src/MessageSendingDemo.py
+++ src/MessageSendingDemo.py
@@ -10,13 +10,12 @@ from NetworkObjects.Node import *
 from NetworkObjects.Connection import Connection
 from SimulationLoop import *
 
-
 from Network import Network
 from Packet import Packet
 
 
-# Define a simple step function to see if the simulation works.
 def test_step(network):
+    """Step function for use in the simulation"""
     i = 0
     print "New Step~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     for packet in network.packets.values():
@@ -45,53 +44,8 @@ def test_step(network):
         # else: Undecided.  This may indicate the packet should be removed
         i += 1
 
-def two_node_demo():
-    #First off, we need a network.
-
-    teh_matrix = Network()
-    #Sweeet.  Now we need a couple of host nodes.
-
-    host = Host()
-    hostess = Host()
-    teh_matrix.add_node(host)
-    teh_matrix.add_node(hostess)
-
-    #Gender equality ftw.  Let's connect 'em up with some arbitrary latency.
-
-    wire = Connection(host, hostess, 5)
-    teh_matrix.add_connection(host, hostess, wire)
-
-    #Okay, now we have a shoddy network.  Next we need a message to send.
-    #UDP seems easy. Let's do that.
-    #Source is host, destination is hostess (Using node_id's as "port numbers"
-    #Set the header length to zero just to be a defecatory disruptor.
-    uheader = UDPHeader(host.node_id, hostess.node_id, 0)
-    segment = UDPSegment(uheader, "I hates ur gutz")
-
-    #Put the segment in an IP datagram with a useless header
-    ip_datagram = IPDatagram("Dis be a IP header", segment)
-
-    #Put the datagram in an ethernet frame with a useless header
-    eth_frame = EthernetFrame("Dis be a ethernet header", ip_datagram)
-
-    #Put the frame in a Packet so the simulation knows what to do with it.
-    packet = Packet(host, eth_frame)
-
-    #Artificially set the packet's connection to the connection between host and hostess
-    packet.set_connection(wire)
-
-    #Add packet to the packets list
-    teh_matrix.packets[packet.packet_id] = packet
-
-    #Create a SimThread that will run a little longer than the connection latency.
-    simulation = SimThread(test_step, teh_matrix, 10)
-
-    print "Starting simulation"
-    simulation.start()
-    simulation.join()
-    print "Simulation all done now =)"
-
 def n_node_demo(n):
+    """Creates a linear network of n Hosts, and sends a single Packet along the network."""
 
     # First off, we need a network.
 
